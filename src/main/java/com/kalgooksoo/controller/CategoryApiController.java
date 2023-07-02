620874dda13b2955b043183c02a9393db00a8865
김도엽 <miro3721@plani.co.kr>
2023-07-02 16:40:22 +0900

    feat: CategoryApiController 테스트 예제 추가

---
+++ src/main/java/com/kalgooksoo/controller/CategoryApiController.java
@@ -0,0 +1,45 @@
+package com.kalgooksoo.controller;
+
+import com.kalgooksoo.criteria.CategoryCriteria;
+import com.kalgooksoo.model.Category;
+import com.kalgooksoo.service.CategoryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryApiController {
+
+    private CategoryService categoryService;
+
+    public CategoryApiController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Collection<Category>> findAll(CategoryCriteria criteria) {
+        return ResponseEntity.ok(this.categoryService.findByPage(criteria));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Category> find(@PathVariable Long id) {
+        return ResponseEntity.ok(this.categoryService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<String> save(@RequestBody Category category) {
+        this.categoryService.insert(category);
+        return ResponseEntity.ok("ok");
+    }
+
+    @PostMapping("/init")
+    public ResponseEntity<String> init() {
+        for (int i = 0; i < 100; i++) {
+            this.save(new Category("name"));
+        }
+        return ResponseEntity.ok("ok");
+    }
+
+}
