5d2ee424a57da3ba39aee3ab437277d57012ad61
김도엽 <miro3721@plani.co.kr>
2023-07-08 23:34:51 +0900

    feat: CRUDMapper.java 수정 Page 정보를 담은 객체 타입으로 반환하는 조회 기본 메서드 추가

--- src/main/java/com/kalgooksoo/mapper/CRUDMapper.java
+++ src/main/java/com/kalgooksoo/mapper/CRUDMapper.java
@@ -1,14 +1,30 @@
 package com.kalgooksoo.mapper;
 
-import com.kalgooksoo.criteria.Criteria;
+import com.kalgooksoo.core.Page;
+import com.kalgooksoo.criteria.PageRequest;
 
 import java.util.List;
 
 public interface CRUDMapper<T, ID> {
+
     void insert(T entity);
+
     List<T> findAll();
+
     T findById(ID id);
-    List<T> findByPage(Criteria<T> criteria);
+
+    List<T> findByPage(PageRequest pageRequest);
+
+    int totalRecordCount(PageRequest pageRequest);
+
     void update(T entity);
+
     void delete(ID id);
+
+    default Page<T> find(PageRequest pageRequest) {
+        List<T> content = this.findByPage(pageRequest);
+        int totalRecordCount = this.totalRecordCount(pageRequest);
+        return new Page<T>(content, totalRecordCount, pageRequest.getCurrentPageNo(), pageRequest.getRecordCountPerPage(), pageRequest.getPageSize());
+    }
+
 }
