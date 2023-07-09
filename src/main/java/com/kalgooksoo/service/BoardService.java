44491ccede2cec2fdcbc40cc26aaece8a741c93e
김도엽 <miro3721@plani.co.kr>
2023-07-09 17:12:09 +0900

    style: BoardService 주석 추가

--- src/main/java/com/kalgooksoo/service/BoardService.java
+++ src/main/java/com/kalgooksoo/service/BoardService.java
@@ -9,6 +9,9 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * 게시글 관리 서비스 객체
+ */
 @Service
 @Transactional
 public class BoardService {
@@ -22,16 +25,35 @@ public class BoardService {
         this.eventPublisher = eventPublisher;
     }
 
+    /**
+     * 조건에 해당하는 게시글 리스트를 반환합니다.
+     *
+     * @param criteria 조건
+     * @return 페이지
+     */
     public Page<Board> findByCategoryId(BoardCriteria criteria) {
 
         // TODO 정렬 처리할 것
         return this.boardMapper.find(criteria);
     }
 
+    /**
+     * 게시글 식별자에 해당하는 게시글을 반환합니다.
+     *
+     * @param id 게시글 식별자
+     * @return 게시글
+     */
     public Board findById(Long id) {
         return this.boardMapper.findById(id);
     }
 
+    /**
+     * 게시글 식별자에 해당하는 게시글을 반환합니다.
+     * 게시글 조회수를 증가시킵니다.
+     *
+     * @param id
+     * @return
+     */
     public Board view(Long id) {
         Board board = this.boardMapper.findById(id);
         this.eventPublisher.publishEvent(new BoardViewedEvent(board));
