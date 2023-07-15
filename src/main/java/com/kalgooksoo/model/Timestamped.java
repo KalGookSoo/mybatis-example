6a66fbbe5c716f09e49a3798d94ea38a30e1d147
김도엽 <miro3721@plani.co.kr>
2023-07-15 22:26:05 +0900

    style: Timestamped.java 주석 추가

--- src/main/java/com/kalgooksoo/model/Timestamped.java
+++ src/main/java/com/kalgooksoo/model/Timestamped.java
@@ -2,32 +2,74 @@ package com.kalgooksoo.model;
 
 import java.time.LocalDateTime;
 
+@SuppressWarnings("unused")
 public class Timestamped {
 
+    /**
+     * 생성일시
+     */
     private LocalDateTime createdAt;
+
+    /**
+     * 수정일시
+     */
     private LocalDateTime updatedAt;
+
+    /**
+     * 삭제일시
+     */
     private LocalDateTime deletedAt;
-    
+
+    /**
+     * 생성일시 접근자
+     *
+     * @return 생성일시
+     */
     public LocalDateTime getCreatedAt() {
         return createdAt;
     }
 
+    /**
+     * 생성일시 설정자
+     *
+     * @param createdAt 생성일시
+     */
     public void setCreatedAt(LocalDateTime createdAt) {
         this.createdAt = createdAt;
     }
 
+    /**
+     * 수정일시 접근자
+     *
+     * @return 수정일시
+     */
     public LocalDateTime getUpdatedAt() {
         return updatedAt;
     }
 
+    /**
+     * 수정일시 설정자
+     *
+     * @param updatedAt 수정일시
+     */
     public void setUpdatedAt(LocalDateTime updatedAt) {
         this.updatedAt = updatedAt;
     }
 
+    /**
+     * 삭제일시 접근자
+     *
+     * @return 삭제일시
+     */
     public LocalDateTime getDeletedAt() {
         return deletedAt;
     }
 
+    /**
+     * 삭제일시 설정자
+     *
+     * @param deletedAt 삭제일시
+     */
     public void setDeletedAt(LocalDateTime deletedAt) {
         this.deletedAt = deletedAt;
     }
