24fdb2adc474d442531c938ee8248c4a438acf92
김도엽 <miro3721@plani.co.kr>
2023-07-16 18:29:37 +0900

    fix: OAuth2UserDetail.java 메서드 접근제한자 제거

--- src/main/java/com/kalgooksoo/security/provider/OAuth2UserDetail.java
+++ src/main/java/com/kalgooksoo/security/provider/OAuth2UserDetail.java
@@ -1,8 +1,8 @@
 package com.kalgooksoo.security.provider;
 
 public interface OAuth2UserDetail {
-    public String getProviderId();
-    public String getProvider();
-    public String getEmail();
-    public String getName();
+    String getProviderId();
+    String getProvider();
+    String getEmail();
+    String getName();
 }
